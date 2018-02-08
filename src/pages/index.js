ef16533c04d3ab6acca7ba571ac781852296b6f3
Mike Allanson <michael.allanson@gmail.com>
2018-02-08 14:47:54 +0000

    Add gatsby-plugin-styled-components

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,8 @@
 import React from "react"
+import styled from 'styled-components'
 
-export default () => <div>Hello world!</div>
+const Hello = styled.div`
+    color: hotpink;
+`
+
+export default () => <Hello>Hello world!</Hello>
